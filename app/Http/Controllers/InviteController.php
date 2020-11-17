5d559fca53784c54b9c8d1c019473521576d06b8
qingji <qingji.liu@beats.net.cn>
2020-11-17 11:10:56 +0800

    windows文件

---
+++ app/Http/Controllers/InviteController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class InviteController extends Controller
+{
+    //用户邀请
+    public function user_invite(Request $request)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -20,17 +20,25 @@ use App\Http\Controllers\Api as AA;
 //     return $request->user();
 // });
 
+Route::group(['prefix'=>'invite'],function(){
+    Route::get('user_invite',[CC\InviteController::class,'user_invite']);
+});
+
+
+
+
+
 //'namespace'=>'App\Http\Controllers'
-Route::group(['namespace'=>'23322323CC\A322323232323A'],function(){
-	Route::get('log',[CC\ApiController::class,'log']);//要不就这样
-	// Route::get('log',"ApiController@log");
-	// Route::get('log',[ApiController::class,'log']);//这个不行
-});
-
-Route::group(['middleware' => 'auth.jwt'], function() {
-    Route::get('user', [CC\ApiController::class,'getAuthUser']);
-});
-
-Route::get('logout', [CC\ApiController::class,'logout']);
-Route::get('user', [CC\ApiController::class,'getAuthUser']);
+//Route::group(['namespace'=>'23322323CC\A322323232323A'],function(){
+//	Route::get('log',[CC\ApiController::class,'log']);//要不就这样
+//	// Route::get('log',"ApiController@log");
+//	// Route::get('log',[ApiController::class,'log']);//这个不行
+//});
+//
+//Route::group(['middleware' => 'auth.jwt'], function() {
+//    Route::get('user', [CC\ApiController::class,'getAuthUser']);
+//});
+//
+//Route::get('logout', [CC\ApiController::class,'logout']);
+//Route::get('user', [CC\ApiController::class,'getAuthUser']);
 
